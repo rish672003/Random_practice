d9812d2e3bbacbb0a55a05b4b377280ae8a4f6da
Rishabh Sharma <rishabhsharma8734@gmail.com>
2025-05-23 20:07:16 +0530

    commit on 23-04-2025

--- Practice/kthelementFinder.cpp
+++ Practice/kthelementFinder.cpp
@@ -2,15 +2,23 @@
 
 using namespace std;
 
-    int findingElement(vector<int> arr(), int k){
 
-        //using the quick sort in this for sorting the array 
-        
-        
 
+    vector<int> quicksort(vector<int> arr , int low , int high ){
 
     }
+
+    
 int main(){
+
+    vector<int> arr = {2,8,9,45,67,89,34,12,10};
+   
+    int kthval = findingElement(arr,7);
+    quicksort()
+
+
+    cout<< kthval;
+
 
     return 0;
 
